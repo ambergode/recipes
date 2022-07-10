1a898f4504f8022f2e3dba3b10e3766d2c0c6a85
James Oakley <james@electronstudio.org>
2022-07-10 13:32:17 -0400

    consistent naming with what was in Amber's repo for djangotemplates

--- tempt/settings.py
+++ tempt/settings.py
@@ -81,7 +81,7 @@ ROOT_URLCONF = 'tempt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'django_templates/')],
+        'DIRS': [os.path.join(BASE_DIR, 'djangotemplates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
