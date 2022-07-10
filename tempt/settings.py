78088a537aba4eaeb3e403a73f061914fd025641
James Oakley <james@electronstudio.org>
2022-07-10 13:26:13 -0400

    hack: rename to templates to django_templates to make ansible happier

--- tempt/settings.py
+++ tempt/settings.py
@@ -81,7 +81,7 @@ ROOT_URLCONF = 'tempt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates/')],
+        'DIRS': [os.path.join(BASE_DIR, 'django_templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
